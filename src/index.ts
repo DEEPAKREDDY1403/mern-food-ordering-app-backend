491756acadb48d76b373e3ed553889b3299a2378
DEEPAKEDDY1403 <deepakreddy217@gmail.com>
2025-01-27 19:48:55 +0530

    adding user profilr feature

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,18 @@ import express from "express";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import { Response,Request } from "express";
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then (()=> console.log("connected to database!"));
+import {v2 as cloudinary} from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then (()=> console.log("connected to database!"));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,7 +24,7 @@ app.get("/health", async (req:Request, res: Response)=>{
     res.send({message: "health OK!"});
 })
 app.use("/api/my/user", myUserRoute);
-
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(4000,()=>{
     console.log("server started on localhost:4000");
